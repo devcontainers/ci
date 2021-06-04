b0b1789c5629e9357b0d9e4d231b244c04691dce
Stuart Leeks <stuart@leeks.net>
2021-06-04 16:00:25 +0000

    Test adding docker and invoking

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as actions_exec from '@actions/exec'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
+
+    console.log("Hello 👋")
+    const r = await exec("docker", "version")
+    console.log(`Got exit code: ${r.exitCode}`)
+    console.log(`Stdout: ${r.stdout}`)
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
@@ -13,6 +20,36 @@ async function run(): Promise<void> {
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
+  }
+}
+
+interface ExecResponse {
+  exitCode: number;
+  stdout: string;
+  stderr: string;
+}
+async function exec(command: string, ...args: string[]): Promise<ExecResponse> {
+
+  let stdout: string = "";
+  let stderr: string = "";
+
+  const options: actions_exec.ExecOptions = {
+    ignoreReturnCode: true,
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout += data.toString()
+      },
+      stderr: (data: Buffer) => {
+        stderr += data.toString()
+      }
+    }
+  }
+  const exitCode = await actions_exec.exec(command, args, options)
+
+  return {
+    exitCode,
+    stdout,
+    stderr
   }
 }
 
