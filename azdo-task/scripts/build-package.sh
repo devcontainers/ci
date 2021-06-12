59267296114cabf7116f392c1e1c8844961f8ca1
Stuart Leeks <stuart@leeks.net>
2021-06-12 15:54:32 +0000

    Change to LICENSE.md

--- azdo-task/scripts/build-package.sh
+++ azdo-task/scripts/build-package.sh
@@ -66,7 +66,7 @@ npm prune --production
 figlet Package extension
 cd "$script_dir/../"
 cp "$script_dir/../../README.md" "$script_dir/../"
-cp "$script_dir/../../LICENSE" "$script_dir/../LICENSE.txt"
+cp "$script_dir/../../LICENSE" "$script_dir/../LICENSE.md"
 echo "Build stuartleeks extension vsix..."
 tfx extension create --manifests vss-extension.json --override "{\"public\": true}"
 echo "Build stuartleeks-dev extension vsix..."
