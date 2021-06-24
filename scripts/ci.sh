a6c94ddeb98825e8dd8e3525f57020f05a591406
Stuart Leeks <stuart@leeks.net>
2021-06-24 07:11:40 +0100

    Switch AzDO org and project for CI build

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -32,9 +32,9 @@ vsix_file=$(ls azdo-task/stuartleeks-dev.*.vsix)
 echo "Using VSIX_FILE=$vsix_file"
 
 # Publish as non-public and as stuartleeks-dev
-tfx extension publish  --token $AZDO_TOKEN --vsix $vsix_file --override "{\"public\": false, \"publisher\": \"stuartleeks-dev\"}" --share-with devcontainer-build-run,stuartle
+tfx extension publish  --token $AZDO_TOKEN --vsix $vsix_file --override "{\"public\": false, \"publisher\": \"stuartleeks-dev\"}" --share-with devcontainer-build-run
 
-tfx extension install  --token $AZDO_TOKEN --vsix $vsix_file --service-url https://dev.azure.com/stuartle
+tfx extension install  --token $AZDO_TOKEN --vsix $vsix_file --service-url $AZDO_ORG
 
 "$script_dir/../azdo-task/scripts/run-azdo-build.sh" --organization $AZDO_ORG --project $AZDO_PROJECT --build $AZDO_BUILD
 
