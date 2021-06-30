b1c198eac3aa40c0d23409fc5b82c1a397437580
Stuart Leeks <stuart@leeks.net>
2021-06-30 13:03:14 +0000

    set permissions for tfx

--- scripts/publish-azdo-task.sh
+++ scripts/publish-azdo-task.sh
@@ -3,6 +3,8 @@ set -e
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+sudo chown -R $(whoami) ~ # TODO - remove this
+
 cd "$script_dir/.."
 echo "Publishing extension..."
 vsix_file=$(ls output/stuartleeks.*.vsix)
