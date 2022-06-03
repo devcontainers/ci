dab40a183fb0f55d2583717926f5b616b2e38c32
Stuart Leeks <stuart@leeks.net>
2022-06-03 09:31:38 +0100

    Add scripts to clean PR images

--- .github/workflows/clean_tags.sh
+++ .github/workflows/clean_tags.sh
@@ -39,7 +39,25 @@ if [[ -z $tag ]]; then
 	exit 1
 fi
 
-image_name="stuartleeks/devcontainer-build-run/tests/dockerfile-context"
+image_names=(
+	"devcontainer-build-run-devcontainer"
+	"devcontainer-build-run/tests/run-args"
+	"devcontainer-build-run/tests/build-args"
+	"devcontainer-build-run/tests/dockerfile-context"
+	"devcontainer-build-run/tests/feature-docker-from-docker"
+	"devcontainer-build-run/tests/docker-from-docker-non-root"
+	"devcontainer-build-run/tests/docker-from-docker-root"
+	"devcontainer-build-run/tests/skip-user-update"
+)
 
-tag_id=$( curl -s  -H "Authorization: Bearer $GITHUB_TOKEN"  -H "Accept: application/vnd.github.v3+json"   https://api.github.com/users/stuartleeks/packages/container/devcontainer-build-run%2ftests%2fdockerfile-context/versions | jq -r '.[] | select(.metadata.container.tags | index("pr-119")) | .id')
-echo $tag_id
+for image_name in ${image_names[@]}; 
+do
+	escaped_image_name=$(echo ${image_name} | sed "s/\//%2f/g")
+	version_id=$( curl -s  -H "Authorization: Bearer $GITHUB_TOKEN"  -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/stuartleeks/packages/container/$escaped_image_name/versions" | jq -r ".[] | select(.metadata.container.tags | index(\"${tag}\")) | .id")
+	if [[ -n $version_id ]]; then
+		echo "Found version '$version_id' for '$image_name:$tag' - deleting..."
+	curl -s -X DELETE -H "Authorization: Bearer $GITHUB_TOKEN"  -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users/stuartleeks/packages/container/$escaped_image_name/versions/$version_id"
+	else
+		echo "Tag '$tag' not found for '$image_name:$tag' - skipping"
+	fi
+done
