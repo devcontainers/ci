7cafdc7e59ee61304102a313d80c80afc25da28d
Stuart Leeks <stuart@leeks.net>
2022-06-08 07:56:00 +0000

    Make private extension visible to new AzDO org

--- scripts/test-azdo.sh
+++ scripts/test-azdo.sh
@@ -18,7 +18,7 @@ echo "Using VSIX_FILE=$vsix_file"
 
 # Publish as non-public and as devcontainers-dev
 echo "Publishing private extension version..."
-tfx extension publish  --token "$MARKETPLACE_TOKEN" --vsix "$vsix_file" --override "{\"public\": false, \"publisher\": \"devcontainers-dev\"}" --share-with devcontainer-build-run,stuartle
+tfx extension publish  --token "$MARKETPLACE_TOKEN" --vsix "$vsix_file" --override "{\"public\": false, \"publisher\": \"devcontainers-dev\"}" --share-with devcontainers-ci,stuartle
 
 echo "Installing private extension"
 tfx extension install  --token "$AZDO_TOKEN" --vsix "$vsix_file" --service-url "$AZDO_ORG"
