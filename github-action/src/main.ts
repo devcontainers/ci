b96cea0815dc12e2d2536e94eca774012dce20fa
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-08-30 16:49:50 -0400

    Limit runCmdOutput size to <1MB

--- github-action/src/main.ts
+++ github-action/src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import truncate from 'truncate-utf8-bytes';
 import path from 'path';
 import {exec} from './exec';
 import {
@@ -133,6 +134,11 @@ export async function runMain(): Promise<void> {
 							`Dev container exec: ${result.message} (exit code: ${result.code})\n${result.description}`,
 						);
 						core.setFailed(result.message);
+					}
+					core.setOutput('runCmdOutput', execLogString);
+					if (Buffer.byteLength(execLogString, 'utf-8') > 1000000) {
+						execLogString = truncate(execLogString, 999966);
+						execLogString += 'TRUNCATED TO 1 MB MAX OUTPUT SIZE';
 					}
 					core.setOutput('runCmdOutput', execLogString);
 					return result;
