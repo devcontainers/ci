12cd12a2e18f494cd141a511a39aa242194d0b32
Stuart Leeks <stuartle@microsoft.com>
2022-08-31 07:42:09 +0000

    bump to 'latest' CLI version

--- github-action/dist/index.js
+++ github-action/dist/index.js
@@ -3740,7 +3740,7 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
 
 
 
-const cliVersion = "0.14.1";
+const cliVersion = "latest"; // Use 'latest' to get latest CLI version, or pin to specific version e.g. '0.14.1' if required
 function getSpecCliInfo() {
     // // TODO - this is temporary until the CLI is installed via npm
     // // TODO - ^ could consider an `npm install` from the folder
