477a5ef747071e150db1772923fc30e76e25592f
CI build <automated-build@example.com>
2023-02-24 14:24:14 +0000

    release

--- github-tests/Dockerfile/docker-from-docker-root/scripts/go-tools.sh
+++
@@ -1,7 +0,0 @@
-go get github.com/go-delve/delve/cmd/dlv@v1.6.0
-# --> Go language server
-go get golang.org/x/tools/gopls@v0.6.6 \
-# --> Go symbols and outline for go to symbol support and test support 
-go get github.com/acroca/go-symbols@v0.1.1 && go get github.com/ramya-rao-a/go-outline@7182a932836a71948db4a81991a494751eccfe77 \
-# --> GolangCI-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0
