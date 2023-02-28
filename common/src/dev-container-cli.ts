f9e1b0acdd12190331f0319e8d0810cd9807fa8f
stephenandary <stephen@jamminmusic.com>
2023-02-28 06:15:47 -0500

    WIP(DevContainerCliUpArgs) - add back missing lines 188, 195, and 200

--- common/src/dev-container-cli.ts
+++ common/src/dev-container-cli.ts
@@ -188,6 +188,7 @@ export interface DevContainerCliUpArgs {
   workspaceFolder: string;
   additionalCacheFroms?: string[];
   skipContainerUserIdUpdate?: boolean;
+  env?: string[];
   userDataFolder?: string;
   additionalMounts?: string[];
 }
@@ -195,10 +196,12 @@ async function devContainerUp(
   args: DevContainerCliUpArgs,
   log: (data: string) => void,
 ): Promise<DevContainerCliUpResult | DevContainerCliError> {
+  const remoteEnvArgs = getRemoteEnvArray(args.env);
   const commandArgs: string[] = [
     'up',
     '--workspace-folder',
     args.workspaceFolder,
+    ...remoteEnvArgs,
   ];
   if (args.additionalCacheFroms) {
     args.additionalCacheFroms.forEach(cacheFrom =>
