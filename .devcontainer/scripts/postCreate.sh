7d2e7e64f02db79af84cda9396831cbfcb13910c
sireesha <sireeshajonnalagadda@github.com>
2025-07-04 13:22:55 +0530

    testing post create script

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo npm install --location=global yarn
+# This script runs after the container is created
+echo 'Post-create script running (/.devcontainer/scripts)...'
+echo "Current folder: $(pwd)"
+echo "Current user: $(whoami)"
